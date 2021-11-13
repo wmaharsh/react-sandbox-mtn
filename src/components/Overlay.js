415d6bf9ffeb23336a027ed303353f92caee4dce
wmaharsh <wmaharsh@gmail.com>
2021-11-13 18:26:42 +0545

    Initial commit

---
+++ src/components/Overlay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Overlay() {
+    return(
+        <div style={{position:'absolute',pointerEvents:'none',top:'0',left:'0',width:'100%',height:'100%'}}>
+            <a href = "https://www.github.com/wmaharsh/" style={{position:'absolute',bottom:'40px',left:'90px',fontSize:'14px'}}>
+                wmaharsh
+            </a>
+        </div>
+    )
+}
